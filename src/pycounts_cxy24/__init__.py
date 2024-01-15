78362a82e7a4aee4cbc2049a5c0b1af558119ede
cindyzhangxy <czhang@cmmt.ubc.ca>
2024-01-15 11:41:41 -0800

    feat: changed __init__.py

--- src/pycounts_cxy24/__init__.py
+++ src/pycounts_cxy24/__init__.py
@@ -1,3 +1,7 @@
 # read version from installed package
 from importlib.metadata import version
 __version__ = version("pycounts_cxy24")
+
+# populate package namespace
+from pycounts_cxy24.pycounts_cxy24 import count_words
+from pycounts_cxy24.plotting import plot_words
